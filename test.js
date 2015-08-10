32b6fe8d764cd02b13399bb344017ab0e1d006c1
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-10 17:15:08 -0400

    update tests

--- test.js
+++ test.js
@@ -1,38 +1,43 @@
-/*!
- * clone-deep <https://github.com/jonschlinkert/clone-deep>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
-
 /* deps: mocha */
+var assert = require('assert');
 var should = require('should');
-var cloneDeep = require('./');
+var clone = require('./');
 
 describe('cloneDeep()', function () {
-  it('should clone a simple array', function () {
-    cloneDeep(['alpha', 'beta', 'gamma']).should.eql(['alpha', 'beta', 'gamma']);
-  });
-  it('should clone an array with varied keys', function () {
-    var val = [0, 'a', {}, [{}], [function() {}], function() {}];
-    cloneDeep(val).should.eql(val);
-  });
-  it('should return primitives directly', function () {
-    cloneDeep(0).should.equal(0);
-    cloneDeep("hello world").should.equal("hello world");
-  });
   it('should clone arrays', function () {
-    cloneDeep([1, 2, 3]).should.eql([1, 2, 3]);
+    assert.deepEqual(clone(['alpha', 'beta', 'gamma']), ['alpha', 'beta', 'gamma']);
+    assert.deepEqual(clone([1, 2, 3]), [1, 2, 3]);
+
+    var a = [{ 'a': 0 }, { 'b': 1 }];
+    var b = clone(a);
+
+    assert.deepEqual(b, a);
+    assert.deepEqual(b[0], a[0]);
+
+    var val = [0, 'a', {}, [{}], [function() {}], function() {}];
+    assert.deepEqual(clone(val), val);
+  });
+  it('should deep clone object', function () {
+    var one = {a: 'b'};
+    var two = clone(one);
+    two.c = 'd';
+    assert.notDeepEqual(one, two);
+  });
+  it('should deep clone arrays', function () {
+    var one = {a: 'b'};
+    var arr1 = [one];
+    var arr2 = clone(arr1);
+    one.c = 'd';
+    assert.notDeepEqual(arr1, arr2);
+  });
+  it('should return primitives', function () {
+    assert.equal(clone(0), 0);
+    assert.equal(clone('foo'), 'foo');
+  });
+  it('should clone a regex', function () {
+    assert.deepEqual(clone(/foo/g), /foo/g);
   });
   it('should clone objects', function () {
-    cloneDeep({a: 1, b: 2, c: 3 }).should.eql({a: 1, b: 2, c: 3 });
-  });
-
-  it('`cloneDeep` deep clone an array of objects.', function() {
-    var expected = [{ 'a': 0 }, { 'b': 1 }];
-    var actual = cloneDeep(expected);
-
-    actual.should.eql.expected;
-    (actual !== expected && actual[0] !== expected[0]).should.be.true;
+    assert.deepEqual(clone({a: 1, b: 2, c: 3 }), {a: 1, b: 2, c: 3 });
   });
 });
