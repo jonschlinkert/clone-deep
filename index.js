0997ec8c63035629d6777a7e8fba4581394ca848
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-07 19:02:13 +0200

    Refactor

--- index.js
+++ index.js
@@ -7,36 +7,16 @@
 const clone = require('shallow-clone');
 const typeOf = require('kind-of');
 
+const TYPES_COLLECTION = ['object', 'array']
+
 function cloneDeep(val, instanceClone) {
-  const type = typeOf(val)
-
-  switch (type) {
-    case 'object':
-      return cloneObjectDeep(val, instanceClone, type);
-    case 'array':
-      return cloneArrayDeep(val, instanceClone);
-    default:
-      return clone(val);
-  }
+  return TYPES_COLLECTION.includes(typeOf(val)) ? cloneCollection(val, instanceClone) : clone(val);
 }
 
-function cloneObjectDeep(val, instanceClone, type) {
-  switch (type) {
-    case 'function':
-      return instanceClone(val);
-    case 'object':
-      const res = new val.constructor();
-      for (const key in val) res[key] = cloneDeep(val[key], instanceClone);
-      return res;
-    default:
-      return val;
-  }
-}
-
-function cloneArrayDeep(val, instanceClone) {
-  const res = new val.constructor(val.length);
-  for (let i = 0; i < val.length; i++) res[i] = cloneDeep(val[i], instanceClone);
-  return res;
+function cloneCollection(val, instanceClone) {
+    const res = new val.constructor();
+    for (const key in val) res[key] = cloneDeep(val[key], instanceClone);
+    return res;
 }
 
 /**
