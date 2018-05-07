21e1873cd217d16b65fb3d6116962083e6af25bb
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-07 18:53:13 +0200

    Pass type and avoid recalculate

--- index.js
+++ index.js
@@ -8,36 +8,34 @@ const clone = require('shallow-clone');
 const typeOf = require('kind-of');
 
 function cloneDeep(val, instanceClone) {
-  switch (typeOf(val)) {
+  const type = typeOf(val)
+
+  switch (type) {
     case 'object':
-      return cloneObjectDeep(val, instanceClone);
+      return cloneObjectDeep(val, instanceClone, type);
     case 'array':
       return cloneArrayDeep(val, instanceClone);
-    default: {
+    default:
       return clone(val);
-    }
   }
 }
 
-function cloneObjectDeep(val, instanceClone) {
-  if (typeof instanceClone === 'function') {
-    return instanceClone(val);
+function cloneObjectDeep(val, instanceClone, type) {
+  switch (type) {
+    case 'function':
+      return instanceClone(val);
+    case 'object':
+      const res = new val.constructor();
+      for (const key in val) res[key] = cloneDeep(val[key], instanceClone);
+      return res;
+    default:
+      return val;
   }
-  if (typeOf(val) === 'object') {
-    const res = new val.constructor();
-    for (const key in val) {
-      res[key] = cloneDeep(val[key], instanceClone);
-    }
-    return res;
-  }
-  return val;
 }
 
 function cloneArrayDeep(val, instanceClone) {
   const res = new val.constructor(val.length);
-  for (let i = 0; i < val.length; i++) {
-    res[i] = cloneDeep(val[i], instanceClone);
-  }
+  for (let i = 0; i < val.length; i++) res[i] = cloneDeep(val[i], instanceClone);
   return res;
 }
 
