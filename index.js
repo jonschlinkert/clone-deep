22fcf6f47311a4fc06718603663bd28f9c179933
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-08 19:21:34 +0200

    Add custom clone support

--- index.js
+++ index.js
@@ -7,16 +7,17 @@
 const clone = require('shallow-clone');
 const typeOf = require('kind-of');
 
-const TYPES_COLLECTION = ['object', 'array']
+const TYPES_COLLECTION = ['object', 'array'];
 
-function cloneDeep(val) {
-  return TYPES_COLLECTION.includes(typeOf(val)) ? cloneCollection(val) : clone(val);
+function cloneDeep(val, fn) {
+  return TYPES_COLLECTION.includes(typeOf(val)) ? cloneCollection(val, fn) : clone(val);
 }
 
 function cloneCollection(val) {
+  if (typeof instanceClone === 'function') return instanceClone(val);
   const res = new val.constructor();
   for (const key in val) {
-    res[key] = cloneDeep(val[key]);
+    res[key] = cloneDeep(val[key], instanceClone);
   }
   return res;
 }
