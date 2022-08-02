7e8d955ea0be8caa2253c19e026b872b24e5dda7
Emmanuel Mahuni <emahuni@gmail.com>
2022-08-02 14:45:18 +0200

    feat: add new circular ref cloning for objects

--- index.js
+++ index.js
@@ -7,8 +7,12 @@
 const clone = require('shallow-clone');
 const typeOf = require('kind-of');
 const isPlainObject = require('is-plain-object');
+const whatsCircular = require('whats-circular');
+const includes = require('lodash.includes');
+const get = require('lodash.get');
 
-function cloneDeep(val, instanceClone) {
+
+function cloneDeep (val, instanceClone) {
   switch (typeOf(val)) {
     case 'object':
       return cloneObjectDeep(val, instanceClone);
@@ -20,27 +24,36 @@ function cloneDeep(val, instanceClone) {
   }
 }
 
-function cloneObjectDeep(val, instanceClone) {
+
+function cloneObjectDeep (val, instanceClone) {
   if (typeof instanceClone === 'function') {
     return instanceClone(val);
   }
   if (instanceClone || isPlainObject(val)) {
     const res = new val.constructor();
+    const circulars = (whatsCircular(val) ?? []).map(c => get(val, c));
+
     for (let key in val) {
-      res[key] = cloneDeep(val[key], instanceClone);
+      if (includes(circulars, val[key])) {
+        res[key] = clone(val[key]);
+      } else {
+        res[key] = cloneDeep(val[key], instanceClone);
+      }
     }
     return res;
   }
   return val;
 }
 
-function cloneArrayDeep(val, instanceClone) {
+
+function cloneArrayDeep (val, instanceClone) {
   const res = new val.constructor(val.length);
   for (let i = 0; i < val.length; i++) {
     res[i] = cloneDeep(val[i], instanceClone);
   }
   return res;
 }
+
 
 /**
  * Expose `cloneDeep`
