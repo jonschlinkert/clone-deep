1f3adf703e8fa0ccf1de5a9d2fd1b85c7c1ae4c0
Emmanuel Mahuni <emahuni@gmail.com>
2022-08-03 06:33:07 +0200

    perf: combine array and object cloning functions into 1

--- index.js
+++ index.js
@@ -35,12 +35,9 @@ function cloneDeep (val, instanceClone) {
 
 function _cloneDeep (val, instanceClone, circulars, circularsPaths, parentPath) {
   switch (typeOf(val)) {
+    case 'array':
     case 'object':
       return cloneObjectDeep(val, instanceClone, circulars, circularsPaths, parentPath);
-      break;
-    case 'array':
-      return cloneArrayDeep(val, instanceClone, circulars, circularsPaths, parentPath);
-      break;
     default: {
       return clone(val);
     }
@@ -49,11 +46,12 @@ function _cloneDeep (val, instanceClone, circulars, circularsPaths, parentPath) 
 
 
 function cloneObjectDeep (val, instanceClone, circulars, circularsPaths, parentPath) {
-  if (typeof instanceClone === 'function') {
+  if (typeof instanceClone === 'function' && !Array.isArray(val)) {
     return instanceClone(val);
   }
-  if (instanceClone || isPlainObject(val)) {
-    const res = new val.constructor();
+
+  if (instanceClone || Array.isArray(val) || isPlainObject(val)) {
+    const res = new val.constructor(Array.isArray(val) ? val.length : undefined);
 
     for (let key in val) {
       const keyPath = parentPath.concat([key]);
@@ -63,31 +61,17 @@ function cloneObjectDeep (val, instanceClone, circulars, circularsPaths, parentP
         circ.parentPath = keyPath;
       } else if ((circ = find(circulars, { path: keyPath }, 0))) {
         goDeeper = false;
-        // res[key] = get(cloned, circ.path); // no access to main obj
       }
 
       if (goDeeper) {
         res[key] = _cloneDeep(val[key], instanceClone, circulars, circularsPaths, keyPath);
       }
     }
+
     return res;
   }
+
   return val;
-}
-
-
-function cloneArrayDeep (val, instanceClone) {
-  const res = new val.constructor(val.length);
-  const circulars = (whatsCircular(val) || []).map(c => get(val, c));
-
-  for (let i = 0; i < val.length; i++) {
-    if (includes(circulars, val[i])) {
-      res[i] = clone(val[i]);
-    } else {
-      res[i] = _cloneDeep(val[i], instanceClone);
-    }
-  }
-  return res;
 }
 
 
